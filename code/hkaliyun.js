ee20d50d38fbe3cdc7c0b1af41a1b679ef90faaa
Yuanzhe Liu <liuyuanzhe0515@gmail.com>
2022-07-15 08:33:18 +0000

    feat: 添加非Text文件支持

--- code/hkaliyun.js
+++ code/hkaliyun.js
@@ -20,4 +20,33 @@ module.exports = (app) => {
             bot.sendMessage(msg.chat.id, 'I can only understand text messages!');
         }
     });
+    bot.on('video',msg => {
+        bot.sendMessage(msg.chat.id, 'I reveive video message!');
+        bot.sendMessage(msg.chat.id, JSON.stringify(msg.video));
+    });
+    bot.on('photo',msg => {
+        bot.sendMessage(msg.chat.id, 'I reveive photo message!');
+        bot.sendMessage(msg.chat.id, JSON.stringify(msg.photo));
+    });
+    bot.on('audio',msg => {
+        bot.sendMessage(msg.chat.id, 'I reveive audio message!');
+        bot.sendMessage(msg.chat.id, JSON.stringify(msg.audio));
+    });
+    bot.on('document',msg => {
+        bot.sendMessage(msg.chat.id, 'I reveive document message!');
+        bot.sendMessage(msg.chat.id, JSON.stringify(msg.document));
+    });
+    bot.on('sticker',msg => {
+        bot.sendMessage(msg.chat.id, 'I reveive sticker message!');
+        bot.sendMessage(msg.chat.id, JSON.stringify(msg.sticker));
+    });
+    bot.on('location',msg => {
+        bot.sendMessage(msg.chat.id, 'I reveive location message!');
+        bot.sendMessage(msg.chat.id, JSON.stringify(msg.location));
+    });
+    bot.on('contact',msg => {
+        bot.sendMessage(msg.chat.id, 'I reveive contact message!');
+        bot.sendMessage(msg.chat.id, JSON.stringify(msg.contact));
+    });
+    
 }
